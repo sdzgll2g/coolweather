9576433d7f4d56389bd8c44cf595a6d32b242ad7
sdzgll2g <1032381616@qq.com>
2015-08-31 22:51:10 +0800

    完成遍历省市县三级列表的功能。

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -12,49 +12,49 @@ public class Utility {
 	* 解析和处理服务器返回的省级数据
 	*/
 	public synchronized static boolean handleProvincesResponse(CoolWeatherDB
-	coolWeatherDB, String response) {
-	if (!TextUtils.isEmpty(response)) {
-	String[] allProvinces = response.split(",");
-	if (allProvinces != null && allProvinces.length > 0) {
-	for (String p : allProvinces) {
-	String[] array = p.split("\\|");
-	Province province = new Province();
-	province.setProvinceCode(array[0]);
-	province.setProvinceName(array[1]);
-	// 将解析出来的数据存储到Province表
-	coolWeatherDB.saveProvince(province);
-	}
-	return true;
-	}
-	}
-	return false;
-	}
-	/**
-	* 解析和处理服务器返回的市级数据
-	*/
-	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,
-	String response, int provinceId) {
-	if (!TextUtils.isEmpty(response)) {
-	String[] allCities = response.split(",");
-	if (allCities != null && allCities.length > 0) {
-	for (String c : allCities) {
-	String[] array = c.split("\\|");
-	City city = new City();
-	city.setCityCode(array[0]);
-	city.setCityName(array[1]);
-	city.setProvinceId(provinceId);
-	// 将解析出来的数据存储到City表
-	coolWeatherDB.saveCity(city);
-	}
-	return true;
-	}
-	}
-	return false;
-	}
-	/**
-	* 解析和处理服务器返回的县级数据
-	*/
-	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
+			coolWeatherDB, String response) {
+			if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+			for (String p : allProvinces) {
+			String[] array = p.split("\\|");
+			Province province = new Province();
+			province.setProvinceCode(array[0]);
+			province.setProvinceName(array[1]);
+			// 将解析出来的数据存储到Province表
+			coolWeatherDB.saveProvince(province);
+			}
+			return true;
+			}
+			}
+			return false;
+			}
+			/**
+			* 解析和处理服务器返回的市级数据
+			*/
+			public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int provinceId) {
+			if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null && allCities.length > 0) {
+			for (String c : allCities) {
+			String[] array = c.split("\\|");
+			City city = new City();
+			city.setCityCode(array[0]);
+			city.setCityName(array[1]);
+			city.setProvinceId(provinceId);
+			// 将解析出来的数据存储到City表
+			coolWeatherDB.saveCity(city);
+			}
+			return true;
+			}
+			}
+			return false;
+			}
+			/**
+			* 解析和处理服务器返回的县级数据
+			*/
+			public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
 			String response, int cityId) {
 			if (!TextUtils.isEmpty(response)) {
 			String[] allCounties = response.split(",");
@@ -73,5 +73,5 @@ public class Utility {
 			}
 			return false;
 			}
+			}
 
-}
