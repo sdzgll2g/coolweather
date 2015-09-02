ccdf344a7c2fed3bc4441714cc234b97c91c0c60
sdzgll2g <1032381616@qq.com>
2015-09-03 00:54:29 +0800

    修改程序图标和名称。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -32,6 +32,7 @@ public class ChooseAreaActivity extends Activity{
 	public static final int LEVEL_PROVINCE = 0;
 	public static final int LEVEL_CITY = 1;
 	public static final int LEVEL_COUNTY = 2;
+	
 	private ProgressDialog progressDialog;
 	private TextView titleText;
 	private ListView listView;
@@ -62,11 +63,18 @@ public class ChooseAreaActivity extends Activity{
 	* 当前选中的级别
 	*/
 	private int currentLevel;
+	/**
+	 * 是否从WeatherActivity中跳转过来。
+	 */
+	private boolean isFromWeatherActivity;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (prefs.getBoolean("city_selected", false)) {
+		// 已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if (prefs.getBoolean("city_selected", false)
+			&& !isFromWeatherActivity){
 		Intent intent = new Intent(this, WeatherActivity.class);
 		startActivity(intent);
 		finish();
@@ -239,6 +247,10 @@ public class ChooseAreaActivity extends Activity{
 		} else if (currentLevel == LEVEL_CITY) {
 			queryProvinces();
 		} else {
+			if (isFromWeatherActivity) {
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
